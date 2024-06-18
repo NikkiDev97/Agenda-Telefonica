7ce4257e29aca2e9d3b9ac2089aa141922d06950
saramaria06 <sara.cerquera@genstudents.org>
2024-06-18 10:33:04 -0500

    clase contactos

--- src/Contactos.java
+++ src/Contactos.java
@@ -1,2 +1,25 @@
 public class Contactos {
+    private String nombre;
+    private long telefono;
+
+    public Contactos(String nombre, long telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
 }
